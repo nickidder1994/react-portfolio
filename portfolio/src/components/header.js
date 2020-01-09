c8f37d1426824af2ca59b3b2efea789457b41128
Nick Kidder <nickkidder@Nicks-MacBook-Pro.local>
2020-01-08 20:11:20 -0800

    added bootstrap

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -2,9 +2,17 @@ import React from "react";
 
 function Name() {
     return (
-        <div>
-            <h1> Nick Kidder</h1>
+        <div className = "container">
+            <div className = "jumbotron">
+                <h1> Nick Kidder</h1>
+                    <nav className="navbar navbar-dark bg-dark">
+                        <button type="button" class="btn btn-dark">About Me</button>
+                        <button type="button" class="btn btn-dark">Portfolio</button>
+                        <button type="button" class="btn btn-dark">Contact</button>    
+                    </nav>
+            </div>
         </div>
+        
     )
 }
 export default Name;
