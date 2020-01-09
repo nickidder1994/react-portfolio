bf6853b097c7bb179546a8782448a3ba6b5e6527
Nick Kidder <nickkidder@Nicks-MacBook-Pro.local>
2020-01-08 19:13:35 -0800

    added h1

---
+++ portfolio/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Name from "./components/header";
+
+function App() {
+  return (
+   <Name />
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Name() {
+    return (
+        <div>
+            <h1> Nick Kidder</h1>
+        </div>
+    )
+}
+export default Name;
