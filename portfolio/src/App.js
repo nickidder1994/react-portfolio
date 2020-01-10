68f6e6654ae71ce4227baaacf8ffb10f8ccf1b94
Nick Kidder <nickkidder@Nicks-MacBook-Pro.local>
2020-01-09 17:59:48 -0800

    added profile picture

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Name from "./components/header";
-import pTag from "./components/header"
-
 function App() {
   return (
    <Name />
--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Mern from "./images/6392882854363136.svg"
+import self from "./images/IMG_4325.jpeg"
+
 
 function Name() {
     return (
@@ -13,6 +15,7 @@ function Name() {
                     </nav>
             </div>
             <div className="jumbotron">
+                <img src={self} alt="" width="120" height="150"/>
                 <p>Hello my name is Nicholas Kidder, I am 25 years old and currently I'm working as a metal fabricator at a sheet metal shop in Fullerton, California but I'm looking to make a career change. 
                     I recieved a certificate from the Coding Bootcamp at University of California, Irvine in November of 2019 that says I have met the requirements to become a full stack MERN developer. My interest in development started in April 2019, I started working on some of my own research and by the following month I was hooked.I dove in head first and registered for the coding bootcamp and from there my interest and passion only grew. 
                     Now that I have completed the Bootcamp, I am continuing my education and working towards recieving my degree in computer science. Continuing my education is one of the biggest aspects of tech and web developmene contt that attracted me to it so much. I truly enjoy learning new skills and I know that development is a field that is constantly growing and changing and it is a career that involves a lifetime of learning. 
@@ -20,7 +23,7 @@ function Name() {
                     I see my self as a people person and found that I get along with everyone and I always make an effort to be someone who is easy to get along and work with.</p>
             </div>
             <div className="jumbotron">
-                <img src={Mern} />
+                <img src={Mern} alt="" width="450"/>
             </div>
         </div>
     )
